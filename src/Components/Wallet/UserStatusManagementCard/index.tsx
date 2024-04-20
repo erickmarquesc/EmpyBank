d488b04b5ae27b227fbae0566522fd82599bf8fc
Erick Marques <erick.marquesc@hotmail.com>
2024-04-20 18:04:36 -0300

    feat: :sparkles: Create a new component

---
+++ src/Components/Wallet/UserStatusManagementCard/index.tsx
@@ -0,0 +1,77 @@
+import {
+  UserStatusManagementCardContent,
+  UserStatusManagementCardHeader,
+  UserStatusManagementCardTable
+} from "./styles"
+
+import {
+  PiArrowCircleRight,
+  PiArrowCircleLeft,
+  PiPlusCircle,
+} from "react-icons/pi"
+
+interface IUserStatusManagementCardProps {
+  title: string,
+  type: 'customer' | 'assistant',
+}
+
+export default function UserStatusManagementCard({ title, type }: IUserStatusManagementCardProps) {
+  return (
+    <UserStatusManagementCardContent>
+      <UserStatusManagementCardHeader type={type}>
+        <section>
+          <h1>{title}</h1>
+          <label>23</label>
+        </section>
+
+        <div>
+          <button className="customer">
+            <PiPlusCircle size={18} />
+            Adicionar cliente
+          </button>
+          <button className="customer vincular">
+            <PiArrowCircleRight size={18} />
+            Vincular
+          </button>
+          <button className="assistant">
+            <PiArrowCircleLeft size={18} />
+            Desvincular
+          </button>
+        </div>
+      </UserStatusManagementCardHeader>
+
+      <input />
+
+      <UserStatusManagementCardTable>
+        <thead>
+          <tr>
+            <th><input type="checkbox" /></th>
+            <th>Código</th>
+            <th>Parceiro</th>
+            <th>Rede</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="first-child"><input type="checkbox" /></td>
+            <td className="code-child">XX30-2</td>
+            <td>Parceiro 1</td>
+            <td className="last-child">Rede A</td>
+          </tr>
+          <tr>
+            <td className="first-child"><input type="checkbox" /></td>
+            <td className="code-child">XX30-2</td>
+            <td>Parceiro 1</td>
+            <td className="last-child">Rede A</td>
+          </tr>
+          <tr>
+            <td className="first-child"><input type="checkbox" /></td>
+            <td className="code-child">XX30-2</td>
+            <td>Parceiro 1</td>
+            <td className="last-child">Rede A</td>
+          </tr>
+        </tbody>
+      </UserStatusManagementCardTable>
+    </UserStatusManagementCardContent>
+  )
+}
---
+++ src/Components/Wallet/UserStatusManagementCard/styles.ts
@@ -0,0 +1,114 @@
+import styled, { css } from "styled-components"
+
+export const UserStatusManagementCardContent = styled.div`
+  flex: 1;
+  gap: 16px;
+  display: flex;
+  padding: 24px;
+  min-width: 470px;
+  border-radius: 18px;
+  flex-direction: column;
+  background-color: ${(props) => props.theme['white']};
+
+  input{
+    width: 100%;
+  };
+`
+interface IUserStatusManagementCardHeaderProps{
+  type: 'customer' | 'assistant',
+}
+export const UserStatusManagementCardHeader = styled.div<IUserStatusManagementCardHeaderProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  section{
+    gap: 16px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    h1{
+      font-size: 1.6rem;
+    };
+
+    label{
+      font-size: 1.4rem;
+      color: ${(props) => props.theme['blue']};
+    };
+  };
+
+  div{
+    gap: 8px;
+    display: flex;
+
+    button{
+      gap: 4px;
+      border: none;
+      display: flex;
+      padding: 4px 16px;
+      font-size: 1.2rem;
+      border-radius: 18px;
+      align-items: center;
+      justify-content: center;
+      color: ${(props) => props.theme['white']};
+      background-color: ${(props) => props.theme['blue']};
+  
+      ${(props) => props.type === 'customer'
+        ? css`
+          &.vincular{
+            background-color: ${(props) => props.theme['purple']};
+          };
+          &.assistant { 
+            display: none;
+          }`
+        : css`
+          background-color: ${(props) => props.theme['orange']};
+          &.customer {
+            display: none;
+          }`
+      };
+    };
+  };
+`
+export const UserStatusManagementCardTable = styled.table`
+  width: 100%;
+  border-spacing: 0 12px;
+  border-collapse: separate;
+
+  th, td {
+    padding: 8px;
+    text-align: left; 
+    font-size: 1.4rem;
+  };
+
+  th {
+    color: rgba(18, 25, 41, 0.64);
+  };
+
+  tbody {
+    tr {
+      td {
+        background-color: ${(props) => props.theme['gray']};
+
+        &.first-child {
+          width: 10%;
+          border-radius: 18px 0 0 18px;
+
+          :hover{
+            cursor: pointer;
+          };
+        };
+
+        &.last-child {
+          width: 15%;
+          border-radius: 0 18px 18px 0;
+        };
+
+        &.code-child {
+          width: 10%;
+        };
+      };
+    };
+  };
+`
