3d0139888712d53ec6779b1e3854601f866fd255
Erick Marques <erick.marquesc@hotmail.com>
2024-04-20 18:03:31 -0300

    feat: :tada: Begin a project
    Ao iniciar o projeto alguma configurações foram necessárias em relação a estruturas de pastas e nomes de arquivos.

---
+++ src/styles/globals.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 10px;
+    -webkit-font-smootming: 'antialiased';
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+  }
+  
+  body {
+    background: ${(props) => props.theme['gray']};
+  }
+  
+  body, input, textarea, button, strong  {
+    font-size: 1.6rem; //16px
+  }
+
+  /* Estilos para a barra de rolagem */
+/* Largura da barra de rolagem */
+::-webkit-scrollbar {
+  width: 10px; /* Você pode ajustar a largura conforme necessário */
+}
+
+/* Rastreamento da barra de rolagem */
+::-webkit-scrollbar-track {
+  background: ${(props) => props.theme['gray']}; /* Cor de fundo */
+}
+
+/* Alça da barra de rolagem */
+::-webkit-scrollbar-thumb {
+  background: ${(props) => props.theme['base-blue']}; /* Cor da alça */
+}
+
+/* Alça da barra de rolagem ao passar o mouse */
+::-webkit-scrollbar-thumb:hover {
+  cursor: pointer;
+  filter: brightness(1.9);
+}
+
+`
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+
+  /* BASE COLORS */
+  'black': '#000000',
+  'white': '#FFFFFF',
+
+  'green': '#00875F',
+  'green-light': '#00B37E',
+  'green-dark': '#015F43',
+
+  'red': '#AB222E',
+  'red-light': '#F75A68',
+  'red-dark': '#7A1921',
+
+  /* COMPONENTS COLORS */
+  'gray': '#EAEDEE',
+
+  'blue': '#00BDFF',
+
+  'purple': '#5E17F5',
+  
+  'orange': '#FF4E3A', 
+  
+} as const
