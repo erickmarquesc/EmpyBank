acfd6b09645c83f3e3b35f86d444a0b0fd7ee82c
Erick Marques <erick.marquesc@hotmail.com>
2024-04-21 17:19:35 -0300

    feat: :sparkles: Cria o contexto para o modal

---
+++ src/Context/index.tsx
@@ -0,0 +1,59 @@
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState
+} from 'react'
+
+
+type UserStatusManagementProps = 'customer' | 'assistant'
+
+interface IModalContextProps {
+  userStatus: UserStatusManagementProps,
+  isOpen: boolean,
+  modalSetIsOpen: () => void,
+  userStatusManagementChange: (method: UserStatusManagementProps) => void
+}
+
+interface IModalContextProviderProps {
+  children: ReactNode,
+}
+
+export const ModalContext = createContext<IModalContextProps>({} as IModalContextProps)
+
+export function ModalContextProvider({ children }: IModalContextProviderProps) {
+
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [userStatus, setuserStatus] = useState<UserStatusManagementProps>('customer')
+
+
+  function userStatusManagementChange(method: UserStatusManagementProps) {
+    setuserStatus(method)
+  }
+
+  function modalSetIsOpen() {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <>
+      <ModalContext.Provider
+        value={{
+          userStatus,
+          isOpen,
+          modalSetIsOpen,
+          userStatusManagementChange
+        }}
+      >
+        {children}
+      </ModalContext.Provider>
+    </>
+  )
+}
+
+// HOOK
+export function useModal() {
+  return useContext(ModalContext)
+}
