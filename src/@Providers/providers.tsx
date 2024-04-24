8901f1f363cb957db0c962d04b0409798b4f9759
Erick Marques <erick.marquesc@hotmail.com>
2024-04-24 13:31:47 -0300

    perf: :zap: Add provider queryClient

--- src/@Providers/providers.tsx
+++ src/@Providers/providers.tsx
@@ -2,8 +2,11 @@ import { ContainerGrid } from "@/Components/ContainersAndContent/styles";
 import Modal from "@/Components/Modal";
 import Sidebar from "@/Components/Sidebar";
 import { ModalContextProvider } from "@/Context";
+import { AssistantContextProvider } from "@/Context/AssistantContext";
+import { queryClient } from "@/lib/react-query";
 import { GlobalStyle } from "@/styles/globals";
 import { defaultTheme } from "@/styles/theme/default";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 
@@ -19,7 +22,11 @@ export default function EmpyBankProviders({ children }: IProvidersProps) {
         <Modal />
         <ContainerGrid>
           <Sidebar />
-          {children}
+          <QueryClientProvider client={queryClient}>
+            <AssistantContextProvider>
+              {children}
+            </AssistantContextProvider>
+          </QueryClientProvider>
         </ContainerGrid>
       </ModalContextProvider>
     </ThemeProvider>
