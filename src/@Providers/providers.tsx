269a496d3c533b6f29e1a6199a50d2024c2a5fa6
Erick Marques <erick.marquesc@hotmail.com>
2024-04-25 17:55:40 -0300

    perf: :art: format of the code eslint

--- src/@Providers/providers.tsx
+++ src/@Providers/providers.tsx
@@ -1,18 +1,18 @@
-import { QueryClientProvider } from "@tanstack/react-query"
-import { queryClient } from "@/lib/react-query"
-import { ReactNode } from "react"
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
+import { ReactNode } from 'react'
 
-import Sidebar from "@/Components/Sidebar"
-import Modal from "@/Components/Modal"
+import Sidebar from '@/Components/Sidebar'
+import Modal from '@/Components/Modal'
 
-import { Container, Content } from "@/Components/ContainersAndContents/styles"
-import { AssistantContextProvider } from "@/Context/AssistantContext"
-import { CustomerContextProvider } from "@/Context/CustomerContext"
-import { ModalContextProvider } from "@/Context/ModalContext"
+import { Container, Content } from '@/Components/ContainersAndContents/styles'
+import { AssistantContextProvider } from '@/Context/AssistantContext'
+import { CustomerContextProvider } from '@/Context/CustomerContext'
+import { ModalContextProvider } from '@/Context/ModalContext'
 
-import { defaultTheme } from "@/styles/theme/default"
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "@/styles/globals"
+import { defaultTheme } from '@/styles/theme/default'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '@/styles/globals'
 
 interface IProvidersProps {
   children: ReactNode,
