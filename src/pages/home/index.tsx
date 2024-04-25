d9da5da13e8562087135c1156d501b97812168bc
Erick Marques <erick.marquesc@hotmail.com>
2024-04-25 18:56:28 -0300

    perf: :zap: Add next-seo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,29 @@
-import UserStatusManagementCard from "@/Components/Wallet/UserStatusManagementCard"
-import { HomeContent, HomeContentUserStatusManagementCard } from "./styles"
-import WalletHeader from "@/Components/Wallet/Header"
+import UserStatusManagementCard from '@/Components/Wallet/UserStatusManagementCard'
+import { HomeContent, HomeContentUserStatusManagementCard } from './styles'
+import WalletHeader from '@/Components/Wallet/Header'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <HomeContent>
+    <>
+      <NextSeo
+        title='Empy | Wallet'
+        description='Soluções financeiras criativas e inteligentes.'
+      />
+      <HomeContent>
 
-      <WalletHeader/>
+        <WalletHeader />
 
-      <HomeContentUserStatusManagementCard>
-        <UserStatusManagementCard
-          title="Cliente (Não vinculado)"
-          type="customer" />
-        <UserStatusManagementCard
-          title="Carteira de:"
-          type="assistant" />
-      </HomeContentUserStatusManagementCard>
+        <HomeContentUserStatusManagementCard>
+          <UserStatusManagementCard
+            title="Cliente (Não vinculado)"
+            type="customer" />
+          <UserStatusManagementCard
+            title="Carteira de:"
+            type="assistant" />
+        </HomeContentUserStatusManagementCard>
 
-    </HomeContent>
+      </HomeContent>
+    </>
   )
 }
